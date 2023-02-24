b3a5e6bf93f72bc680e35b4e8a5fa27c13e6c68e
Timothy Atobatele <oladimeji.atobatele@gmail.com>
2023-02-24 05:54:03 +0100

    corction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,26 +6,19 @@ def validUTF8(data):
     return True if data is valid UTF-8 encoding
     else return False
     """
-    count = 0
-    if len(data) == 0:
-        return True
-
-    for c in data:
-        bit = 1 << 7
-        if count != 0:
-            if not (c & 1 << 7 and not (c & 1 << 6)):
+     num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            while (bit & c):
-                count += 1
-                bit = bit >> 1
-
-            if count == 0:
-                continue
-            if count == 1 or count > 4:
+            if byte >> 6 != 0b10:
                 return False
-        count -= 1
-
-    if count == 0:
-        return True
-    return False
+        num_bytes -= 1
+    return num_bytes == 0
