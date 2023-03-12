324d178a34786f18407ed0f2ec313cb24a4b3489
Timothy Atobatele <oladimeji.atobatele@gmail.com>
2023-03-13 00:11:42 +0100

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
 
-/**
- * Write a script that prints all characters of a Star Wars movie:
-
-    * The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
-    * Display one character name per line in the same order as the “characters” list in the /films/ endpoint
-    * You must use the Star wars API
-    * You must use the request module
-    * /
-
 const req = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
